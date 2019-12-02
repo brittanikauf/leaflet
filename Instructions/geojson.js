e36b7547337cc5d2f72b2ac0ed7c3a474ccfde79
brittanikauf <42155831+brittanikauf@users.noreply.github.com>
2019-12-02 12:45:23 -0500

    Add files via upload

---
+++ Instructions/geojson.js
@@ -0,0 +1,141 @@
+{
+    "type": "FeatureCollection",
+    "metadata": {
+    "generated": 1575307142000,
+    "url": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
+    "title": "USGS Significant Earthquakes, Past Week",
+    "status": 200,
+    "api": "1.8.1",
+    "count": 3
+    },
+    "features": [
+    {
+    "type": "Feature",
+    "properties": {
+    "mag": 6,
+    "place": "41km NW of Platanos, Greece",
+    "time": 1574839422552,
+    "updated": 1575206399312,
+    "tz": 120,
+    "url": "https://earthquake.usgs.gov/earthquakes/eventpage/us70006dlt",
+    "detail": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us70006dlt.geojson",
+    "felt": 107,
+    "cdi": 5.7,
+    "mmi": 4.462,
+    "alert": "green",
+    "status": "reviewed",
+    "tsunami": 0,
+    "sig": 615,
+    "net": "us",
+    "code": "70006dlt",
+    "ids": ",us70006dlt,",
+    "sources": ",us,",
+    "types": ",dyfi,geoserve,ground-failure,losspager,moment-tensor,origin,phase-data,shakemap,",
+    "nst": null,
+    "dmin": 1.394,
+    "rms": 1.16,
+    "gap": 23,
+    "magType": "mww",
+    "type": "earthquake",
+    "title": "M 6.0 - 41km NW of Platanos, Greece"
+    },
+    "geometry": {
+    "type": "Point",
+    "coordinates": [
+    23.2673,
+    35.7272,
+    71.76
+    ]
+    },
+    "id": "us70006dlt"
+    },
+    {
+    "type": "Feature",
+    "properties": {
+    "mag": 5.4,
+    "place": "6km ESE of Blagaj, Bosnia and Herzegovina",
+    "time": 1574759966080,
+    "updated": 1575284316393,
+    "tz": 60,
+    "url": "https://earthquake.usgs.gov/earthquakes/eventpage/us70006d4h",
+    "detail": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us70006d4h.geojson",
+    "felt": 184,
+    "cdi": 6.8,
+    "mmi": 7.347,
+    "alert": "yellow",
+    "status": "reviewed",
+    "tsunami": 0,
+    "sig": 775,
+    "net": "us",
+    "code": "70006d4h",
+    "ids": ",us70006d4h,",
+    "sources": ",us,",
+    "types": ",dyfi,geoserve,losspager,moment-tensor,origin,phase-data,shakemap,",
+    "nst": null,
+    "dmin": 1.253,
+    "rms": 1.08,
+    "gap": 23,
+    "magType": "mww",
+    "type": "earthquake",
+    "title": "M 5.4 - 6km ESE of Blagaj, Bosnia and Herzegovina"
+    },
+    "geometry": {
+    "type": "Point",
+    "coordinates": [
+    17.9617,
+    43.2415,
+    10
+    ]
+    },
+    "id": "us70006d4h"
+    },
+    {
+    "type": "Feature",
+    "properties": {
+    "mag": 6.4,
+    "place": "16km WSW of Mamurras, Albania",
+    "time": 1574736852594,
+    "updated": 1575301019292,
+    "tz": 60,
+    "url": "https://earthquake.usgs.gov/earthquakes/eventpage/us70006d0m",
+    "detail": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us70006d0m.geojson",
+    "felt": 1345,
+    "cdi": 9.1,
+    "mmi": 8.279,
+    "alert": "orange",
+    "status": "reviewed",
+    "tsunami": 0,
+    "sig": 1910,
+    "net": "us",
+    "code": "70006d0m",
+    "ids": ",us70006d0m,",
+    "sources": ",us,",
+    "types": ",dyfi,general-text,geoserve,ground-failure,impact-text,losspager,moment-tensor,origin,phase-data,shakemap,",
+    "nst": null,
+    "dmin": 0.937,
+    "rms": 0.58,
+    "gap": 17,
+    "magType": "mww",
+    "type": "earthquake",
+    "title": "M 6.4 - 16km WSW of Mamurras, Albania"
+    },
+    "geometry": {
+    "type": "Point",
+    "coordinates": [
+    19.5151,
+    41.5112,
+    20
+    ]
+    },
+    "id": "us70006d0m"
+    }
+    ],
+    "bbox": [
+    17.9617,
+    35.7272,
+    10,
+    23.2673,
+    43.2415,
+    71.76
+    ]
+    }
